0d4916d24952cd7588e3b60384af63f70660d5e3
Luciano J. Ferreyra <luciano.ferreyra@diveria.com>
2025-10-20 22:36:13 -0300

    Remove commented basePath and assetPrefix from Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    basePath: '/ui',
-    assetPrefix: '/ui',
+    // basePath: '/ui',
 };
 export default nextConfig;
